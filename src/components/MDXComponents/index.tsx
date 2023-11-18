ef3a77c416b0368319646894016b57663afec0f6
Kisan <kisan@fullness.io>
2023-11-18 22:59:37 +0530

    WIP: Blog - Google captcha in Remix.js

--- src/components/MDXComponents/index.tsx
+++ src/components/MDXComponents/index.tsx
@@ -1,7 +1,7 @@
 import { ElementType, ReactNode } from "react";
 import { MDXProvider } from "@mdx-js/react";
 
-import { H2, P } from "../UI/Typography";
+import { H2, H3, P } from "../UI/Typography";
 
 interface ComponentProps {
   as: ElementType;
@@ -33,10 +33,33 @@ export const components: Components = {
       {children}
     </Component>
   ),
+  h3: ({ children, ...rest }) => (
+    <Component as={H3} className="mt-8" {...rest}>
+      {children}
+    </Component>
+  ),
   pre: ({ children, ...rest }) => (
     <Component
       as="pre"
       className="mb-4 text-base overflow-hidden rounded-lg border border-line"
+      {...rest}
+    >
+      {children}
+    </Component>
+  ),
+  code: ({ children, ...rest }) => (
+    <Component
+      as="code"
+      className="mb-4 text-[14px] px-1 overflow-hidden rounded-md border border-line"
+      {...rest}
+    >
+      {children}
+    </Component>
+  ),
+  a: ({ children, ...rest }) => (
+    <Component
+      as="a"
+      className="text-inherit underline decoration-primary underline-offset-2"
       {...rest}
     >
       {children}
